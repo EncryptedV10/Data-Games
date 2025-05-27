5d7fe62a914a6826e7d9e536bac7b9a014380b62
Encrypted <marwanlecourtabram91670@gmail.com>
2025-05-27 22:33:18 +0200

    Create Mutations.lua

---
+++ Grow A Garden/Mutations.lua
@@ -0,0 +1,67 @@
+return {
+  ["Chilled"] = {
+    Name = "Chilled",
+    ValueMulti = 2,
+    Color = Color3.fromRGB(135, 206, 250)
+  },
+  ["Moonlit"] = {
+    Name = "Moonlit",
+    ValueMulti = 2,
+    Color = Color3.fromRGB(153, 141, 255)
+  },
+  ["Twisted"] = {
+    Name = "Twisted",
+    ValueMulti = 5,
+    Color = Color3.fromRGB(191, 191, 191)
+  },
+  ["Burnt"] = {
+    Name = "Burnt",
+    ValueMulti = 5,
+    Color = Color3.fromRGB(40, 40, 40)
+  },
+  ["Choc"] = {
+    Name = "Choc",
+    ValueMulti = 2,
+    Color = Color3.fromRGB(92, 64, 51)
+  },
+  ["Frozen"] = {
+    Name = "Frozen",
+    ValueMulti = 10,
+    Color = Color3.fromRGB(108, 184, 255)
+  },
+  ["Wet"] = {
+    Name = "Wet",
+    ValueMulti = 2,
+    Color = Color3.fromRGB(64, 164, 223)
+  },
+  ["Shocked"] = {
+    Name = "Shocked",
+    ValueMulti = 100,
+    Color = Color3.fromRGB(255, 255, 100)
+  },
+  ["Bloodlit"] = {
+    Name = "Bloodlit",
+    ValueMulti = 4,
+    Color = Color3.fromRGB(200, 0, 0)
+  },
+  ["Zombified"] = {
+    Name = "Zombified",
+    ValueMulti = 25,
+    Color = Color3.fromRGB(128, 199, 127)
+  },
+  ["Celestial"] = {
+    Name = "Celestial",
+    ValueMulti = 120,
+    Color = Color3.fromRGB(255, 0, 255)
+  },
+  ["Disco"] = {
+    Name = "Disco",
+    ValueMulti = 125,
+    Color = Color3.fromRGB(255, 105, 180)
+  },
+  ["Plasma"] = {
+    Name = "Plasma",
+    ValueMulti = 5,
+    Color = Color3.fromRGB(208, 43, 137)
+  },
+}
